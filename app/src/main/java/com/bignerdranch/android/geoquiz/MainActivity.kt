3d0fd59d764d951acbdc82c9ffcdcd1f0ba38a34
fakhrievii <ifs3112@gmail.com>
2022-10-04 13:25:38 +0300

    Добавил кнопку Next, добавил список вопросов, создал Data класс Questions

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -4,30 +4,62 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.StringRes
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initData()
         setListeners()
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
     }
 
     private fun initData() {
         trueButton = findViewById(R.id.trueButton)
         falseButton = findViewById(R.id.falseButton)
+        nextButton = findViewById(R.id.nextButton)
+        questionTextView = findViewById(R.id.questionTextView)
     }
 
     private fun setListeners() {
-        trueButton.setOnClickListener { showToast(R.string.correct_toast) }
-        falseButton.setOnClickListener { showToast(R.string.incorrect_toast) }
+        trueButton.setOnClickListener { checkAnswer(true) }
+        falseButton.setOnClickListener { checkAnswer(false) }
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
     }
 
-    private fun showToast(textId: Int) {
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        if (userAnswer == correctAnswer) { showToast(R.string.correct_toast)
+        } else { showToast(R.string.incorrect_toast) }
+    }
+
+    private fun showToast(@StringRes textId: Int) {
         val toast = Toast.makeText(this, textId, Toast.LENGTH_SHORT)
         toast.apply {
             setGravity(Gravity.TOP, 0, 0)
