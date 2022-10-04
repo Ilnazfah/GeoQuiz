cd1d21d442d7e9633d38ad9f1049124f8670b1ab
fakhrievii <ifs3112@gmail.com>
2022-10-04 10:44:30 +0300

    1. Первое Android-приложение

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,10 +2,36 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initData()
+        setListeners()
+    }
+
+    private fun initData() {
+        trueButton = findViewById(R.id.trueButton)
+        falseButton = findViewById(R.id.falseButton)
+    }
+
+    private fun setListeners() {
+        trueButton.setOnClickListener { showToast(R.string.correct_toast) }
+        falseButton.setOnClickListener { showToast(R.string.incorrect_toast) }
+    }
+
+    private fun showToast(textId: Int) {
+        val toast = Toast.makeText(this, textId, Toast.LENGTH_SHORT)
+        toast.apply {
+            setGravity(Gravity.TOP, 0, 0)
+            show()
+        }
     }
 }
